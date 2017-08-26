a42dc0fefd8e6fd32fb343910c676fd89bbdaffd
Eduardo Oliveros Acosta <eduardo@Eduardos-MacBook-Pro.local>
2017-08-26 16:43:10 -0500

    travis file and unit test added

---
+++ Chart ImplementationTests/Chart_ImplementationTests.swift
@@ -0,0 +1,35 @@
+//
+//  Chart_ImplementationTests.swift
+//  Chart ImplementationTests
+//
+//  Created by Eduardo Oliveros Acosta on 8/26/17.
+//  Copyright © 2017 Eduardo Oliveros. All rights reserved.
+//
+
+import XCTest
+
+class Chart_ImplementationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
