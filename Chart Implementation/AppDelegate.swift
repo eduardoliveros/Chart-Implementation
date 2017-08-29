6a737c24b33328ab8a1b8ddd5506502122ad722b
Eduardo Oliveros Acosta <eduardo@Eduardos-MacBook-Pro.local>
2017-08-29 00:14:12 -0500

    changes for first PR2

--- Chart Implementation/AppDelegate.swift
+++ Chart Implementation/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-	//Changes for pull request
+	//Changes for pull request2
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
